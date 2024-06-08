7c2f20fd09d75a6dd43c971351283378975d0229
TylerHartwell <thartwell37@gmail.com>
2024-06-08 15:41:25 -0700

    wip uploadthing

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,14 +1,26 @@
 "use client"
 
 import { Progress } from "@/components/ui/progress"
+import { useUploadThing } from "@/lib/uploadthing"
 import { cn } from "@/lib/utils"
 import { Loader2, MousePointerSquareDashed, Image } from "lucide-react"
+import { useRouter } from "next/navigation"
 import { useState, useTransition } from "react"
 import Dropzone, { FileRejection } from "react-dropzone"
 
 const Page = () => {
   const [isDragOver, setIsDragOver] = useState<boolean>(false)
   const [uploadProgress, setUploadProgress] = useState<number>(0)
+  const router = useRouter()
+
+  const {} = useUploadThing("imageUploader", {
+    onClientUploadComplete: ([data]) => {
+      const configId = data.serverData.configId
+      startTransition(() => {
+        router.push(`/configure/design?id=${configId}`)
+      })
+    }
+  })
 
   const onDropRejected = () => {}
   const onDropAccepted = () => {
@@ -56,13 +68,22 @@ const Page = () => {
                     <Progress value={uploadProgress} className="mt-2 w-40 h-2 bg-gray-300" />
                   </div>
                 ) : isPending ? (
-                  <div></div>
+                  <div className="flex flex-col items-center">
+                    <p>Redirecting, please wait...</p>
+                  </div>
                 ) : isDragOver ? (
-                  <span></span>
+                  <p>
+                    <span className="font-semibold">Drop file</span>
+                    to upload
+                  </p>
                 ) : (
-                  <span></span>
+                  <p>
+                    <span className="font-semibold">Click to upload</span> or drag and drop
+                  </p>
                 )}
               </div>
+
+              {isPending ? null : <p className="text-xs text-zinc-500">PNG, JPG, JPEG</p>}
             </div>
           )}
         </Dropzone>
