24a8489135846e292fe29fa326cf17f1d2c892b5
TylerHartwell <thartwell37@gmail.com>
2024-06-12 16:24:47 -0700

    add material finish and prices

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -3,16 +3,17 @@
 import HandleComponent from "@/components/HandleComponent"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 import { ScrollArea } from "@/components/ui/scroll-area"
-import { cn } from "@/lib/utils"
+import { cn, formatPrice } from "@/lib/utils"
 import NextImage from "next/image"
 import { Rnd } from "react-rnd"
-import { Radio, RadioGroup } from "@headlessui/react"
+import { Description, Radio, RadioGroup } from "@headlessui/react"
 import { useState } from "react"
-import { COLORS, MODELS } from "@/validators/option-validator"
+import { COLORS, FINISHES, MATERIALS, MODELS } from "@/validators/option-validator"
 import { Label } from "@/components/ui/label"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
 import { Check, ChevronsUpDown } from "lucide-react"
+import { BASE_PRICE } from "@/config/products"
 
 interface DesignConfiguratorProps {
   configId: string
@@ -27,9 +28,13 @@ const DesignConfigurator = ({ configId, imageUrl, imageDimensions }: DesignConfi
   const [options, setOptions] = useState<{
     color: (typeof COLORS)[number]
     model: (typeof MODELS.options)[number]
+    material: (typeof MATERIALS.options)[number]
+    finish: (typeof FINISHES.options)[number]
   }>({
     color: COLORS[0],
-    model: MODELS.options[0]
+    model: MODELS.options[0],
+    material: MATERIALS.options[0],
+    finish: FINISHES.options[0]
   })
 
   return (
@@ -138,10 +143,68 @@ const DesignConfigurator = ({ configId, imageUrl, imageDimensions }: DesignConfi
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </div>
+
+                {[MATERIALS, FINISHES].map(({ name, options: selectableOptions }) => (
+                  <RadioGroup
+                    key={name}
+                    value={options[name]}
+                    onChange={val => {
+                      setOptions(prev => ({
+                        ...prev,
+                        [name]: val
+                      }))
+                    }}
+                  >
+                    <Label>{name.slice(0, 1).toUpperCase() + name.slice(1)}</Label>
+                    <div className="mt-3 space-y-4">
+                      {selectableOptions.map(option => (
+                        <Radio
+                          key={option.value}
+                          value={option}
+                          className={({ focus, checked }) =>
+                            cn(
+                              "relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between",
+                              {
+                                "border-primary": focus || checked
+                              }
+                            )
+                          }
+                        >
+                          <span className="flex items-center">
+                            <span className="flex flex-col text-sm">
+                              <Label className="font-medium text-gray-900">{option.label}</Label>
+
+                              {option.description ? (
+                                <Description as="span" className="text-gray-500">
+                                  <span className="block sm:inline">{option.description}</span>
+                                </Description>
+                              ) : null}
+                            </span>
+                          </span>
+
+                          <Description as="span" className="mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right">
+                            <span className="font-medium text-gray-900">{formatPrice(option.price / 100)}</span>
+                          </Description>
+                        </Radio>
+                      ))}
+                    </div>
+                  </RadioGroup>
+                ))}
               </div>
             </div>
           </div>
         </ScrollArea>
+
+        <div className="w-full px-8 h-16 bg-white">
+          <div className="h-px w-full bg-zinc-200" />
+          <div className="w-full h-full flex justify-end items-center">
+            <div className="w-full flex gap-6 items-center">
+              <p className="font-medium whitespace-nowrap">
+                {formatPrice((BASE_PRICE + options.finish.price + options.material.price) / 100)}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
