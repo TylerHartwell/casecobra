450a44d96c06205c938267c4c58eaddc4b063639
TylerHartwell <thartwell37@gmail.com>
2024-06-19 22:14:05 -0700

    creating thank you page

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -7,7 +7,7 @@ import Stripe from "stripe"
 export async function POST(req: Request) {
   try {
     const body = await req.text()
-    const signature = headers().get("stripe=signature")
+    const signature = headers().get("stripe-signature")
 
     if (!signature) {
       return new Response("invalid signature", { status: 400 })
