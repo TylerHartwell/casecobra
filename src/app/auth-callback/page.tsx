5214cd6bb9eb0adbb9880775194ba151ce3ffb6e
TylerHartwell <thartwell37@gmail.com>
2024-06-25 14:59:06 -0700

    fix redirect to configuration preview after log in

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -23,16 +23,14 @@ const Page = () => {
     retryDelay: 1000
   })
 
-  useEffect(() => {
-    if (data?.success) {
-      if (configId) {
-        localStorage.removeItem("configurationId")
-        router.push(`/configure/preview?id=${configId}`)
-      } else {
-        router.push("/")
-      }
+  if (data?.success) {
+    if (configId) {
+      localStorage.removeItem("configurationId")
+      router.push(`/configure/preview?id=${configId}`)
+    } else {
+      router.push("/")
     }
-  }, [data, configId, router])
+  }
 
   return (
     <div className="w-full mt-24 flex justify-center">
